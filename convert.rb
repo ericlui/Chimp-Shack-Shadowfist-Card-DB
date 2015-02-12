8a046ba178ef6ac13870dbc2ca228adc53b9adb6
Eric Lui <eric@factual.com>
2015-02-11 16:59:40 -0800

    Removed debugging code, added comments on the totally patchy unicode characters i'm subbing.

--- convert.rb
+++ convert.rb
@@ -3,14 +3,10 @@
 ARGF.each do |line|
   line = line.force_encoding('IBM437').encode('UTF-8')
   line.gsub!(/[\“\”]/, '"')
+  # 2565, 2559 are the munged smart quotes
   line.gsub!(/[\u201C\u201d\u2565\u2559]/, '"')
   # 2552 is the munged apostrophe
   line.gsub!(/[\u00ef\u00bf\u00bd\u2552]/u, "'")
   line.gsub!(/\r\n?/, "\n")
   puts line
-  foo = /(.)Sworn(.) Character token/.match(line)
-  if foo 
-  	warn foo[1].ord.to_s(16)
-    warn foo[2].ord.to_s(16)
-  end
 end
